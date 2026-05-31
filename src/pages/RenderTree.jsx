be5b6d483146e87dc400941f93e68adbc96108f9
Cristina <clmbjcm@gmail.com>
2026-05-31 17:00:38 -0300

    FIX: árbol de renderizado

--- src/pages/RenderTree.jsx
+++ src/pages/RenderTree.jsx
@@ -1,117 +1,72 @@
 function RenderTree() {
-
     return (
-
         <section className="tree-page">
-
             <div className="tree-header">
-
                 <h1>Render Tree</h1>
-
-                <p>
-                    Estructura jerárquica de componentes React.
-                </p>
-
+                <p>Estructura jerárquica de componentes React.</p>
             </div>
 
             <div className="tree-container">
-
-                {/* ROOT */}
-
-                <div className="tree-node root">
-                    App
-                </div>
-
-                <div className="tree-line vertical"></div>
-
-                {/* LEVEL 1 */}
-
-                <div className="tree-level">
-
-                    <div className="tree-node">
-                        Layout
-                    </div>
-
-                </div>
-
-                <div className="tree-line vertical"></div>
-
-                {/* LEVEL 2 */}
-
-                <div className="tree-level">
-
+                <div className="tree-node root">App (React Router)</div>
+                
+                <div className="tree-children">
                     <div className="tree-branch">
-
-                        <div className="tree-node">
-                            Sidebar
-                        </div>
-
-                        <div className="tree-line vertical small"></div>
-
+                        <div className="tree-node level-1">Layout (Theme Provider & Outlet)</div>
+                        
                         <div className="tree-children">
-
-                            <div className="tree-node child">
-                                Home
+                            
+                            <div className="tree-branch">
+                                <div className="tree-node level-2">Sidebar (Navegación Fija)</div>
+                                <div className="tree-children">
+                                    <div className="tree-node child">ThemeToggle (Botón)</div>
+                                    <div className="tree-node child">MenuLinks (Links Router)</div>
+                                </div>
                             </div>
 
-                            <div className="tree-node child">
-                                Bitácora
-                            </div>
+                            <div className="tree-branch">
+                                <div className="tree-node level-2">MainContent (Rutas Dinámicas)</div>
+                                <div className="tree-children">
+                                    
+                                    <div className="tree-branch">
+                                        <div className="tree-node child">Home (Dashboard)</div>
+                                        <div className="tree-children">
+                                            <div className="tree-node sub-child">MemberCard (Tarjetas)</div>
+                                        </div>
+                                    </div>
 
-                            <div className="tree-node child">
-                                Nosotros
-                            </div>
+                                    <div className="tree-branch">
+                                        <div className="tree-node child">Perfil (Ruta Dinámica)</div>
+                                        <div className="tree-children">
+                                            <div className="tree-node sub-child">Carrusel (Proyectos)</div>
+                                            <div className="tree-node sub-child">TechStack (Barras de Progreso)</div>
+                                        </div>
+                                    </div>
 
-                            <div className="tree-node child">
-                                Explorer
-                            </div>
+                                    <div className="tree-branch">
+                                        <div className="tree-node child">Galería</div>
+                                        <div className="tree-children">
+                                            <div className="tree-node sub-child">Grilla (Imágenes)</div>
+                                            <div className="tree-node sub-child">Lightbox (Modal Interactivo)</div>
+                                        </div>
+                                    </div>
 
-                            <div className="tree-node child">
-                                API Explorer
-                            </div>
+                                    <div className="tree-node child">Explorer (Buscador JSON Local)</div>
+                                    
+                                    <div className="tree-node child">Api Explorer (Fetch GitHub API)</div>
 
-                            <div className="tree-node child">
-                                Render Tree
+                                    <div className="tree-branch">
+                                        <div className="tree-node child">Bitácora (Docs)</div>
+                                        <div className="tree-children">
+                                            <div className="tree-node sub-child">BitacoraComponents</div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
                             </div>
-
                         </div>
-
                     </div>
-
-                    <div className="tree-branch">
-
-                        <div className="tree-node">
-                            MainContent
-                        </div>
-
-                        <div className="tree-line vertical small"></div>
-
-                        <div className="tree-children">
-
-                            <div className="tree-node child">
-                                HomePage
-                            </div>
-
-                            <div className="tree-node child">
-                                Explorer
-                            </div>
-
-                            <div className="tree-node child">
-                                ApiExplorer
-                            </div>
-
-                            <div className="tree-node child">
-                                MemberProfile
-                            </div>
-
-                        </div>
-
-                    </div>
-
                 </div>
-
             </div>
-
         </section>
     );
 }
